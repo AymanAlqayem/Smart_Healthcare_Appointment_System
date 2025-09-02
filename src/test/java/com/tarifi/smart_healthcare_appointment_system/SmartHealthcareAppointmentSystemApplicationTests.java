c7ed00fe42ce3b4a2a713b9e57e8116696c2573c
Ayman.Alqayem <nabilayman021@gmai.com>
2025-09-02 08:42:32 +0300

    Initial Spring Boot project

---
+++ src/test/java/com/tarifi/smart_healthcare_appointment_system/SmartHealthcareAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarifi.smart_healthcare_appointment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHealthcareAppointmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
